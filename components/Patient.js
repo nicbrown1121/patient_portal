2ca941d8149ac3c905aa867a9b2c1347fe3ca592
Nicole Brown <nicolebrown@Nicoles-MacBook-Air.local>
2023-06-05 17:18:48 -0400

    filter patient query by id from router

--- components/Patient.js
+++ components/Patient.js
@@ -5,8 +5,54 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 function Patient({ id }) {
   const { user, dispatch } = useContext(UserContext);
   const queryClient = useQueryClient();
-  console.log("id pn Patient.js", id);
-  return <div>Hi</div>;
+
+  console.log("id on Patient.js", typeof id);
+
+  const currPatient = queryClient
+    .getQueryData(["patient"])
+    .data.find((patient) => patient.id === parseInt(id, 10));
+
+  console.log("currPatient", currPatient);
+
+  const handleCreateNote = () => {};
+
+  return (
+    <div>
+      <div className="patientInfo">
+        <h2 style={{ textAlign: "center" }}>{currPatient.name}</h2>
+      </div>
+      <div className="patientPage">
+        <div className="notesColumn">
+          <div className="patientCards">
+            <h2>Notes</h2>
+            <button className="patientButton" onClick={handleCreateNote}>
+              Create Note
+            </button>
+          </div>
+        </div>
+        <div className="rightColumn">
+          <div className="patientCards">
+            <h2>Current Admission</h2>
+            <ol> Admission Date: {currPatient.dateOfAdmission}</ol>
+            <ol> Diagnosis: {currPatient.diagnosis} </ol>
+            <ol> Location: {currPatient.location} </ol>
+          </div>
+          <div className="patientCards">
+            <h2>Anthropometric Data</h2>
+            <ol>Height: 72 inches</ol>
+            <ol>Weight: 178 lbs</ol>
+            <ol> BMI: 27.2 </ol>
+          </div>
+          <div className="patientCards">
+            <h2>Diet </h2>
+            <ol>PO Status: PO </ol>
+            <ol>Diet Order: NAS, renal </ol>
+            <ol>Fluid Restriction: 1200ml </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Patient;
